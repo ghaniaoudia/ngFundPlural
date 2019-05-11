7f1c4725624382cd1070feee397d1517f0bee0ac
Abdelghani AOUDIA <abdelghani.aoudia@outlook.com>
2019-05-11 22:07:02 +0200

    First commit

---
+++ ng-fundamentals/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
+import { ToastrService } from './common/toastr.service';
+import { Router, RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
+import { Error404Component } from './errors/404.component';
+import { EventsListComponent, EventThumbnailComponent, EventDetailsComponent, CreateEventComponent, EventRouteActivator, EventListResolver } from './events/index'
+import { EventsAppComponent } from './events-app.component';
+
+@NgModule({
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarComponent,
+    EventDetailsComponent,
+    CreateEventComponent,
+    Error404Component
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [
+    EventService,
+    ToastrService,
+    EventRouteActivator,
+    EventListResolver,
+    {
+      provide: 'canDeactivateCreateEvent', useValue: checkDirtyState
+    }
+  ],
+  bootstrap: [EventsAppComponent]
+})
+export class AppModule { }
+export function checkDirtyState(component: CreateEventComponent){
+  if(component.isDirty) {
+    return window.confirm('You have not saved this element, do you really want to cancel ?')
+  }
+  return true
+}
---
+++ ng-fundamentals/src/app/events/event-details/event-route-activator.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+import { EventService } from '../shared/event.service';
+import { ActivatedRouteSnapshot, Router, CanActivate } from '@angular/router';
+
+@Injectable()
+export class EventRouteActivator implements CanActivate{
+    constructor(private eventService: EventService,
+        private router: Router ){}
+    canActivate(route: ActivatedRouteSnapshot){
+        const eventExists = !!this.eventService.getEvent(+route.params['id'])//to boolean
+        if(!eventExists) {
+            this.router.navigate(['404']);
+        }
+        return eventExists;
+
+    }
+}
